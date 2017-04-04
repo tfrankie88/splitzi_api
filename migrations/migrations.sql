0f27bd20912e6a796dbef3b05064f242e636c5ce
Tim Frankie <tfrankie88@gmail.com>
2017-04-04 12:31:03 -0400

    created not null input logic for

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS restaurants
 (id BIGSERIAL PRIMARY KEY,
 first_name VARCHAR(255),
 last_name VARCHAR(255),
-email VARCHAR(255),
-password VARCHAR(255),
+email VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
 restaurant_name VARCHAR(255),
 country VARCHAR(255),
 postal INTEGER,
@@ -15,7 +15,7 @@ access BOOLEAN
 
 CREATE TABLE IF NOT EXISTS menus
 (id BIGSERIAL PRIMARY KEY,
-item VARCHAR(255),
-price MONEY,
+item VARCHAR(255) NOT NULL,
+price MONEY NOT NULL,
 restaurant_id INTEGER REFERENCES restaurants(id)
 );
