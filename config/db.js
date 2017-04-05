c6c089925b33d75d649ff5dd216698f5b3d28ce6
Tim Frankie <tfrankie88@gmail.com>
2017-04-04 21:01:30 -0400

    working heroku

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const pgp = require('pg-promise')();
 
-const db = pgp ({
-  host: 'localhost',
-  port: 5432,
-  database: 'splitzi'
-});
+// const db = pgp ({
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'splitzi'
+// });
+
+const connection = process.env.DATABASE_URL;
+
+const db = pgp(connection);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(require('./resources'));
 
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server listening on', PORT)
 });
